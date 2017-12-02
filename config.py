574d506d0ec8a00451634619dfe24990b869ce34
satvikshetty04 <satvikshetty04@gmail.com>
2017-12-02 12:30:22 -0500

    Updated files

---
+++ config.py
@@ -0,0 +1,6 @@
+# Hosts for various AWS components
+
+es_endpoint='search-newssearchengine-rhlbiq3mtga7gj6czbw3box4km.us-east-1.es.amazonaws.com'
+s3_bucket='saadi-baalti'
+kinesis_stream_name='news_stream'
+model_file_name='XGBClassifier.pkl'
